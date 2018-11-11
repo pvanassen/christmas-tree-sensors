7f622c922fbf458aeddfe03fa611061cb0a8bdc6
Paul van Assen <source@pvanassen.nl>
2018-11-11 20:50:21 +0100

    Too much Scala... Why return Python?

--- app/config/Config.py
+++ app/config/Config.py
@@ -15,22 +15,22 @@ class Config:
         self._config.read('config/user.ini')
 
     def get_bus(self):
-        int(self._config.get('BH1750', 'bus'), 0)
+        return int(self._config.get('BH1750', 'bus'), 0)
 
     def get_device(self):
-        int(self._config.get('BH1750', 'device'), 0)
+        return int(self._config.get('BH1750', 'device'), 0)
 
     def get_mode(self):
-        int(self._config.get('BH1750', 'mode'), 0)
+        return int(self._config.get('BH1750', 'mode'), 0)
 
     def get_pin(self):
-        int(self._config.get('DHT11', 'pin'), 0)
+        return int(self._config.get('DHT11', 'pin'), 0)
 
     def get_hostname(self):
-        self._config.get('STATSD', 'hostname')
+        return self._config.get('STATSD', 'hostname')
 
     def get_port(self):
-        int(self._config.get('STATSD', 'port'), 0)
+        return int(self._config.get('STATSD', 'port'), 0)
 
     def get_prefix(self):
-        self._config.get('STATSD', 'prefix')
+        return self._config.get('STATSD', 'prefix')
